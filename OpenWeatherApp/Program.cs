876d54b4bff8ed958fea696393df63488dc7a968
Dwight Caddell <dcaddelljr@gmail.com>
2023-04-12 21:36:06 -0500

    OpenWeatherApp

---
+++ OpenWeatherApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json.Linq;
+
+namespace OpenWeatherApp;
+class Program
+{
+    static void Main(string[] args)
+    {
+        string key = File.ReadAllText("appsettings.json");
+        string APIKey = JObject.Parse(key).GetValue("APIKey").ToString();
+
+        Console.WriteLine("Please enter your zip code.");
+        var zipCode = Console.ReadLine();
+
+        var apiCall = $"https://api.openweathermap.org/data/2.5/weather?units=imperial&apid={APIKey}&zip={zipCode}";
+
+        Console.WriteLine();
+        Console.WriteLine($"The temperature is currently {WeatherMap.GetTemp(apiCall)} at your location.");
+    }
+}
+
---
+++ OpenWeatherApp/WeatherMap.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Newtonsoft.Json.Linq;
+
+namespace OpenWeatherApp
+{
+	public class WeatherMap
+	{
+        public static double GetTemp(string apiCall)
+        {
+            var client = new HttpClient();
+
+            var url = "https://api.kanye.rest/";  //end point. storing as a string to plug into the program
+            var response = client.GetStringAsync(apiCall).Result;
+            var temp = double.Parse(JObject.Parse(response)["main"]["temp"].ToString());
+
+            return temp;
+
+        }
+    }
+}
+
